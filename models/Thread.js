77aa9787259876d634ac54f950bfaad5ddbef2cd
Honsei901 <homma_wic@outlook.jp>
2023-01-30 07:20:59 +0900

    add some

--- models/Thread.js
+++ models/Thread.js
@@ -1,12 +1,95 @@
 const mongoose = require('mongoose');
 
 const ThreadSchema = new mongoose.Schema({
-  title: {
+  awbNo: {
     type: String,
     required: true,
-    maxlength: 20,
   },
-  content: {
+  exchangeCd: {
+    type: String,
+    required: true,
+  },
+  impCode: {
+    type: String,
+    required: true,
+  },
+  impCodeS: {
+    type: String,
+    required: true,
+  },
+  discMark: {
+    type: String,
+    required: true,
+  },
+  impTel: {
+    type: String,
+    required: true,
+  },
+  impZip: {
+    type: String,
+    required: true,
+  },
+  impName: {
+    type: String,
+    required: true,
+  },
+  impAddr01: {
+    type: String,
+    required: true,
+  },
+  itemCode: {
+    type: String,
+    required: true,
+  },
+  countCd: {
+    type: String,
+    required: true,
+  },
+  countPr: {
+    type: String,
+    required: true,
+  },
+  quant1: {
+    type: String,
+    required: true,
+  },
+  quant1Cd: {
+    type: String,
+    required: true,
+  },
+  originCountry: {
+    type: String,
+    required: true,
+  },
+  origName: {
+    type: String,
+    required: true,
+  },
+  origAd01: {
+    type: String,
+    required: true,
+  },
+  accountNum: {
+    type: String,
+    required: true,
+  },
+  ips1: {
+    type: String,
+    required: true,
+  },
+  ips2: {
+    type: String,
+    required: true,
+  },
+  ips3: {
+    type: String,
+    required: true,
+  },
+  ips4: {
+    type: String,
+    required: true,
+  },
+  valuation: {
     type: String,
     required: true,
   },
--- public/script.js
+++ public/script.js
@@ -1,10 +1,52 @@
-const formDOM = document.querySelector('.form-section');
+const formDOM = document.querySelector('.input-form');
 const threadSectionDOM = document.querySelector('.thread-section');
-const inputTextDOM = document.getElementById('inputTitle');
-const inputContentDOM = document.getElementById('inputContent');
+const inputAwbDOM = document.getElementById('awbNo');
+const inputExchangeCdDOM = document.getElementById('exchangeCd');
+const inputImpCodeDOM = document.getElementById('impCode');
+const inputImpCodeSDOM = document.getElementById('impCodeS');
+const inputDiscMarkDOM = document.getElementById('discMark');
+const inputImpTelDOM = document.getElementById('impTel');
+const inputImpZipDOM = document.getElementById('impZip');
+const inputImpNameDOM = document.getElementById('impName');
+const inputImpAddr01DOM = document.getElementById('impAddr01');
+const inputItemCodeDOM = document.getElementById('itemCode');
+const inputCountCdDOM = document.getElementById('countCd');
+const inputCountPrDOM = document.getElementById('countPr');
+const inputQuant1DOM = document.getElementById('quant1');
+const inputQuant1CdDOM = document.getElementById('quant1Cd');
+const inputOriginCountryDOM = document.getElementById('originCountry');
+const inputOrigNameDOM = document.getElementById('origName');
+const inputOrigAd01DOM = document.getElementById('origAd01');
+const inputAccountNumDOM = document.getElementById('accountNum');
+const inputIps1DOM = document.getElementById('ips1');
+const inputIps2DOM = document.getElementById('ips2');
+const inputIps3DOM = document.getElementById('ips3');
+const inputIps4DOM = document.getElementById('ips4');
+const inputvaluationDOM = document.getElementById('valuation');
 
-let inputText = '';
-let inputContentText = '';
+let inputAwbNo = '';
+let inputExchangeCd = '';
+let inputImpCode = '';
+let inputImpCodeS = '';
+let inputDiscMark = '';
+let inputImpTel = '';
+let inputImpZip = '';
+let inputImpName = '';
+let inputImpAddr01 = '';
+let inputItemCode = '';
+let inputCountCd = '';
+let inputCountPr = '';
+let inputQuant1 = '';
+let inputQuant1Cd = '';
+let inputOriginCountry = '';
+let inputOrigName = '';
+let inputOrigAd01 = '';
+let inputAccountNum = '';
+let inputIps1 = '';
+let inputIps2 = '';
+let inputIps3 = '';
+let inputIps4 = '';
+let inputValuation = '';
 
 //Get method
 const getAllThreads = async () => {
@@ -15,13 +57,58 @@ const getAllThreads = async () => {
     //Output
     allThreads = data
       .map((thread) => {
-        const { title, content } = thread;
+        const {
+          awbNo,
+          exchangeCd,
+          impCode,
+          impCodeS,
+          discMark,
+          impTel,
+          impZip,
+          impName,
+          impAddr01,
+          itemCode,
+          countCd,
+          countPr,
+          quant1,
+          quant1Cd,
+          originCountry,
+          origName,
+          origAd01,
+          accountNum,
+          ips1,
+          ips2,
+          ips3,
+          ips4,
+          valuation,
+        } = thread;
 
         //Return as html
         return `
         <div class="single-thread">
-          <h3>${title}</h3>
-          <p>${content}</p>
+          <p>${awbNo}</p>
+          <p>${exchangeCd}</p>
+          <p>${impCode}</p>
+          <p>${impCodeS}</p>
+          <p>${discMark}</p>
+          <p>${impTel}</p>
+          <p>${impZip}</p>
+          <p>${impName}</p>
+          <p>${impAddr01}</p>
+          <p>${itemCode}</p>
+          <p>${countCd}</p>
+          <p>${countPr}</p>
+          <p>${quant1}</p>
+          <p>${quant1Cd}</p>
+          <p>${originCountry}</p>
+          <p>${origName}</p>
+          <p>${origAd01}</p>
+          <p>${accountNum}</p>
+          <p>${ips1}</p>
+          <p>${ips2}</p>
+          <p>${ips3}</p>
+          <p>${ips4}</p>
+          <p>${valuation}</p>
         </div>
       `;
       })
@@ -46,22 +133,85 @@ getAllThreads();
 
 formDOM.addEventListener('submit', async (e) => {
   e.preventDefault();
-  inputText = inputTextDOM.value;
-  inputContentText = inputContentDOM.value;
 
-  if (inputText && inputContentText) {
-    try {
-      await axios
-        .post('/api/v1/thread', {
-          title: inputText,
-          content: inputContentText,
-        })
-        .then(() => getAllThreads());
-    } catch (err) {
-      console.log(err);
-    }
+  inputAwbNo = inputAwbDOM.value;
+  inputExchangeCd = inputExchangeCdDOM.value;
+  inputImpCode = inputImpCodeDOM.value;
+  inputImpCodeS = inputImpCodeSDOM.value;
+  inputDiscMark = inputDiscMarkDOM.value;
+  inputImpTel = inputImpTelDOM.value;
+  inputImpZip = inputImpZipDOM.value;
+  inputImpName = inputImpNameDOM.value;
+  inputImpAddr01 = inputImpAddr01DOM.value;
+  inputItemCode = inputItemCodeDOM.value;
+  inputCountCd = inputCountCdDOM.value;
+  inputCountPr = inputCountPrDOM.value;
+  inputQuant1 = inputQuant1DOM.value;
+  inputQuant1Cd = inputQuant1CdDOM.value;
+  inputOriginCountry = inputOriginCountryDOM.value;
+  inputOrigName = inputOrigNameDOM.value;
+  inputOrigAd01 = inputOrigAd01DOM.value;
+  inputAccountNum = inputAccountNumDOM.value;
+  inputIps1 = inputIps1DOM.value;
+  inputIps2 = inputIps2DOM.value;
+  inputIps3 = inputIps3DOM.value;
+  inputIps4 = inputIps4DOM.value;
+  inputValuation = inputvaluationDOM.value;
+
+  try {
+    await axios
+      .post('/api/v1/thread', {
+        awbNo: inputAwbNo,
+        exchangeCd: inputExchangeCd,
+        impCode: inputImpCode,
+        impCodeS: inputImpCodeS,
+        discMark: inputDiscMark,
+        impTel: inputImpTel,
+        impZip: inputImpZip,
+        impName: inputImpName,
+        impAddr01: inputImpAddr01,
+        itemCode: inputItemCode,
+        countCd: inputCountCd,
+        countPr: inputCountPr,
+        quant1: inputQuant1,
+        quant1Cd: inputQuant1Cd,
+        originCountry: inputOriginCountry,
+        origName: inputOrigName,
+        origAd01: inputOrigAd01,
+        accountNum: inputAccountNum,
+        ips1: inputIps1,
+        ips2: inputIps2,
+        ips3: inputIps3,
+        ips4: inputIps4,
+        valuation: inputValuation,
+      })
+      .then(() => getAllThreads());
+    console.log('done!');
+  } catch (err) {
+    console.log(err);
   }
 
-  inputTextDOM.value = null;
-  inputContentDOM.value = null;
+  inputAwbDOM.value = null;
+  inputExchangeCdDOM.value = null;
+  inputImpCodeDOM.value = null;
+  inputImpCodeSDOM.value = null;
+  inputDiscMarkDOM.value = null;
+  inputImpTelDOM.value = null;
+  inputImpZipDOM.value = null;
+  inputImpNameDOM.value = null;
+  inputImpAddr01DOM.value = null;
+  inputItemCodeDOM.value = null;
+  inputCountCdDOM.value = null;
+  inputCountPrDOM.value = null;
+  inputQuant1DOM.value = null;
+  inputQuant1CdDOM.value = null;
+  inputOriginCountryDOM.value = null;
+  inputOrigNameDOM.value = null;
+  inputOrigAd01DOM.value = null;
+  inputAccountNumDOM.value = null;
+  inputIps1DOM.value = null;
+  inputIps2DOM.value = null;
+  inputIps3DOM.value = null;
+  inputIps4DOM.value = null;
+  inputvaluationDOM.value = null;
 });
